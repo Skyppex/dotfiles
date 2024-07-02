d94aed8c36c9ad15b9e80ca25c61fe531f03d689
Skypex <84551201+Skyppex@users.noreply.github.com>
2024-07-03 01:12:07 +0200

    use absolute line numbers

--- nvim/lua/skypex/config.lua
+++ nvim/lua/skypex/config.lua
@@ -24,9 +24,9 @@ vim.opt.wrap = false
 
 -- Make line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
-vim.opt.relativenumber = true
+
+-- Not needed since i use hop to jump around on the viewport
+vim.opt.relativenumber = false
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = "a"
