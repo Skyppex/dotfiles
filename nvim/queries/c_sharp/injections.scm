ead5c3a6be524c7e9077f69a56f18fcef05a1d64
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2024-08-05 16:10:30 +0200

    add injections for verbatim string literals and add regex language for it
    the regex highlighting doesn't work though

--- nvim/queries/c_sharp/injections.scm
+++ nvim/queries/c_sharp/injections.scm
@@ -78,9 +78,43 @@
         (raw_string_literal
             (raw_string_content) @injection.content))))))))))
 
+((comment) @injection.language (#offset! @injection.language 0 8 0 0)
+    (_
+        (verbatim_string_literal) @injection.content))
+
+((comment) @injection.language (#offset! @injection.language 0 8 0 0)
+    (_ (_
+        (verbatim_string_literal) @injection.content)))
+
+((comment) @injection.language (#offset! @injection.language 0 8 0 0)
+    (_ (_ (_
+        (verbatim_string_literal) @injection.content))))
+
+((comment) @injection.language (#offset! @injection.language 0 8 0 0)
+    (_ (_ (_ (_
+        (verbatim_string_literal) @injection.content)))))
+
+((comment) @injection.language (#offset! @injection.language 0 8 0 0)
+    (_ (_ (_ (_ (_
+        (verbatim_string_literal) @injection.content))))))
+
+((comment) @injection.language (#offset! @injection.language 0 8 0 0)
+    (_ (_ (_ (_ (_ (_
+        (verbatim_string_literal) @injection.content)))))))
+
+((comment) @injection.language (#offset! @injection.language 0 8 0 0)
+    (_ (_ (_ (_ (_ (_ (_
+        (verbatim_string_literal) @injection.content))))))))
+
+((comment) @injection.language (#offset! @injection.language 0 8 0 0)
+    (_ (_ (_ (_ (_ (_ (_ (_
+        (verbatim_string_literal) @injection.content)))))))))
+
 (invocation_expression
   function: (member_access_expression
     name: (identifier) @_name (#eq? @_name "Match"))
   arguments: (argument_list
     (argument
-      (verbatim_string_literal) @regex (#offset! @regex 0 2 0 -1))))
+      (verbatim_string_literal) @injection.content
+      (#offset! @injection.content 0 2 0 -1)
+      (#set! "injection.language" "regex"))))
