e89f5ab13d48df1e1e1bf91204a02e1eef652fc6
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2024-11-28 17:35:51 +0100

    wip(nvim): add command and args text object queries for nushell

--- nvim/queries/nu/textobjects.scm
+++ nvim/queries/nu/textobjects.scm
@@ -10,3 +10,21 @@
   body: (block . "{" . (_) @_start @_end (_)? @_end . "}"
     (#make-range! "function.inner" @_start @_end))) @function.outer
 
+((comment) @comment.inner (#offset! @comment.inner 0 1 0 0)) @comment.outer
+
+((parameter_bracks
+  ((parameter) 
+   (comment)?) @parameter.inner @parameter.outer))
+
+((command
+  .
+  flag: (_) @parameter.inner @parameter.outer))
+
+((command
+  .
+  arg_str: (_) @parameter.inner @parameter.outer))
+
+((command
+  .
+  arg: (_) @parameter.inner @parameter.outer))
+
