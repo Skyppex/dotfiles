b49b99b1c2502b2d957f5b93b4ebda91ff1ef31c
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2024-12-09 09:11:41 +0100

    chore(nvim): remove neotest config
    its causing problems rather solving them

--- nvim/lua/skypex/plugins/neotest.lua
+++ nvim/lua/skypex/plugins/neotest.lua
@@ -1,16 +1,16 @@
 return {
-	"nvim-neotest/neotest",
-	-- "neotest",
-	-- dir = require("skypex.utils").get_code_path() .. "/neotest",
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-		"nvim-lua/plenary.nvim",
-		"antoinemadec/FixCursorHold.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		"Issafalcon/neotest-dotnet",
-		"rouge8/neotest-rust",
-	},
-	config = function()
-		require("skypex.custom.neotest")
-	end,
+	-- "nvim-neotest/neotest",
+	-- -- "neotest",
+	-- -- dir = require("skypex.utils").get_code_path() .. "/neotest",
+	-- dependencies = {
+	-- 	"nvim-neotest/nvim-nio",
+	-- 	"nvim-lua/plenary.nvim",
+	-- 	"antoinemadec/FixCursorHold.nvim",
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	"Issafalcon/neotest-dotnet",
+	-- 	"rouge8/neotest-rust",
+	-- },
+	-- config = function()
+	-- 	require("skypex.custom.neotest")
+	-- end,
 }
