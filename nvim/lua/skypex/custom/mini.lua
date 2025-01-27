c461660a0ab2bd823d406de11e465351b82fe052
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-01-27 22:35:50 +0100

    refactor(nvim): use mini.nvim instead of standalone for several plugins

--- nvim/lua/skypex/custom/mini.lua
+++ nvim/lua/skypex/custom/mini.lua
@@ -5,3 +5,56 @@
 --  - yinq - [Y]ank [I]nside [N]ext [']quote
 --  - ci'  - [C]hange [I]nside [']quote
 require("mini.ai").setup({ n_lines = 500 })
+
+-- Commenting
+require("mini.comment").setup()
+
+-- Auto pairs
+require("mini.pairs").setup()
+
+-- Splits and joins
+require("mini.splitjoin").setup({
+	mappings = {
+		toggle = "gs",
+	},
+	detect = {
+		brackets = {
+			"%b()",
+			"%b[]",
+			"%b{}",
+			"%b<>",
+		},
+		separator = ",",
+		exclude_regions = {
+			"%b()",
+			"%b[]",
+			"%b{}",
+			"%b<>",
+			'%b""',
+			"%b''",
+		},
+	},
+})
+
+-- Surround
+require("mini.surround").setup({
+	custom_surroundings = {
+		["{"] = { output = { left = "{ ", right = " }" } },
+		["}"] = { output = { left = "{ ", right = " }" } },
+		["["] = { output = { left = "[", right = "]" } },
+		["]"] = { output = { left = "[", right = "]" } },
+		["("] = { output = { left = "(", right = ")" } },
+		[")"] = { output = { left = "(", right = ")" } },
+		["<"] = { output = { left = "<", right = ">" } },
+		[">"] = { output = { left = "<", right = ">" } },
+	},
+	mappings = {
+		add = "s",
+		delete = "ds",
+		replace = "cs",
+		find = "",
+		find_left = "",
+		highlight = "",
+		update_n_lines = "",
+	},
+})
--- nvim/lua/skypex/plugins/surround.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"kylechui/nvim-surround",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("skypex.custom.surround")
-	end,
-}
