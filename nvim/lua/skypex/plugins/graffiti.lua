a025a217c94a45d149e4a2f6b11ac1e57ca4996e
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-02-24 14:20:11 +0100

    feat(nvim): build graffiti-rs if i'm not using a local version of graffiti.nvim

--- nvim/lua/skypex/plugins/graffiti.lua
+++ nvim/lua/skypex/plugins/graffiti.lua
@@ -1,9 +1,47 @@
 local graffiti_dir = require("skypex.utils").get_code_path() .. "/graffiti.nvim"
 
--- if not require("plenary.scandir").scan_dir(graffiti_dir) then
--- 	vim.notify("Graffiti is not installed")
--- 	return {}
--- end
+local Path = require("plenary.path")
+local path = Path:new(graffiti_dir)
+
+if not path:is_dir() then
+	-- vim.notify("Graffiti not found at: " .. graffiti_dir, vim.log.levels.WARNING)
+	return {
+		"skyppex/graffiti.nvim",
+		name = "graffiti",
+		opts = {
+			server_executable = vim.fn.stdpath("data") .. "/graffiti-rs/target/release/graffiti-rs.exe",
+		},
+		build = function()
+			if vim.fn.executable(vim.fn.stdpath("data") .. "graffiti-rs/target/release/graffiti-rs.exe") ~= 0 then
+				return
+			end
+
+			local Job = require("plenary.job")
+			Job:new({
+				command = "git",
+				args = {
+					"clone",
+					"git@github.com:Skyppex/graffiti-rs.git",
+					vim.fn.stdpath("data") .. "/graffiti-rs",
+				},
+				on_exit = function()
+					Job:new({
+						command = "cargo",
+						args = {
+							"build",
+							"--release",
+							"--manifest-path",
+							vim.fn.stdpath("data") .. "/graffiti-rs/Cargo.toml",
+							"--target-dir",
+							vim.fn.stdpath("data") .. "/graffiti-rs/target",
+						},
+					}):start()
+				end,
+			}):start()
+		end,
+		config = true,
+	}
+end
 
 return {
 	"graffiti",
