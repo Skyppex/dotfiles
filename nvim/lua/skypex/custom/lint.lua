b32f8fba0b503dcc99a7ca2dc47ca45917c27e24
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-06-07 01:29:35 +0200

    feat(nvim): ensure installed linters based on filetype config

--- nvim/lua/skypex/custom/lint.lua
+++ nvim/lua/skypex/custom/lint.lua
@@ -1,20 +1,27 @@
 local M = {}
 
+local linters_by_ft = {
+	go = { "golangcilint" },
+	javascript = { "eslint_d" },
+	typescript = { "eslint_d" },
+	javascriptreact = { "eslint_d" },
+	typescriptreact = { "eslint_d" },
+	json = { "jsonlint" },
+	yaml = { "yamllint" },
+	sh = { "shellcheck" },
+	bash = { "shellcheck" },
+	kotlin = { "ktlint" },
+	-- markdown = { "markdownlint" },
+}
+
+local lint_to_mason = {
+	golangcilint = "golangci-lint",
+}
+
 M.lint = function()
 	local lint = require("lint")
 
-	lint.linters_by_ft = {
-		go = { "golangcilint" },
-		javascript = { "eslint_d" },
-		typescript = { "eslint_d" },
-		javascriptreact = { "eslint_d" },
-		typescriptreact = { "eslint_d" },
-		json = { "jsonlint" },
-		yaml = { "yamllint" },
-		sh = { "shellcheck" },
-		bash = { "shellcheck" },
-		kotlin = { "ktlint" },
-	}
+	lint.linters_by_ft = linters_by_ft
 
 	require("skypex.utils").nmap("<leader>l", function()
 		lint.try_lint()
@@ -31,14 +38,22 @@ M.lint = function()
 end
 
 M.mason = function()
+	local ensure_installed = {}
+
+	for _, linters in pairs(linters_by_ft) do
+		for _, linter in pairs(linters) do
+			if type(linter) == "table" then
+				for _, l in pairs(linter) do
+					ensure_installed[l] = 1
+				end
+			else
+				ensure_installed[linter] = 1
+			end
+		end
+	end
+
 	require("mason-nvim-lint").setup({
-		ensure_installed = {
-			"eslint_d",
-			"golangci-lint",
-			"jsonlint",
-			"shellcheck",
-			"yamllint",
-		},
+		ensure_installed = ensure_installed,
 		automatic_installation = true,
 	})
 end
