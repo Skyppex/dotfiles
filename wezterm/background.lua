095d7bdd7fde770650c03f5a546b6431e9aafc9b
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-06-08 18:58:31 +0200

    feat(hypr): add modular theming

--- wezterm/background.lua
+++ wezterm/background.lua
@@ -1,5 +1,6 @@
--- local wezterm = require("wezterm")
+local wezterm = require("wezterm")
 local utils = require("utils")
+local toml = require("toml")
 
 local config_path = utils.get_config_path()
 local background
@@ -15,15 +16,51 @@ if utils.is_work_computer() then
 		hsb = { brightness = 0.02, saturation = 1 },
 	}
 else
-	background = {
-		source = {
-			File = config_path .. "/wezterm/backgrounds/nier 2b 2250x4000.jpg",
-		},
-		width = "100%",
-		vertical_align = "Bottom",
-		repeat_x = "NoRepeat",
-		hsb = { brightness = 0.01 },
-	}
+	if utils.is_home_computer_linux() then
+		-- find current.toml and read the dir value
+		local current_path = config_path .. "/themes/current.toml"
+		local current_file = io.open(current_path, "r")
+		local current_content = current_file:read("*a")
+		current_file:close()
+		local current = toml.parse(current_content)
+		wezterm.log_info(current.dir)
+
+		local theme_config_path = config_path .. "/themes/" .. current.dir .. "/config.toml"
+		local theme_config_file = io.open(theme_config_path, "r")
+		local theme_config_content = theme_config_file:read("*a")
+		theme_config_file:close()
+		local theme_config = toml.parse(theme_config_content)
+		wezterm.log_info(theme_config.variant)
+
+		-- find the correct background for current theme
+		background = {
+			source = {
+				File = config_path
+					.. "/themes/"
+					.. current.dir
+					.. "/images/"
+					.. current.dir
+					.. ":"
+					.. theme_config.variant
+					.. "-term"
+					.. ".png",
+			},
+			width = "100%",
+			vertical_align = "Bottom",
+			repeat_x = "NoRepeat",
+			hsb = { brightness = 0.01 },
+		}
+	else
+		background = {
+			source = {
+				File = config_path .. "/wezterm/backgrounds/nier 2b 2250x4000.jpg",
+			},
+			width = "100%",
+			vertical_align = "Bottom",
+			repeat_x = "NoRepeat",
+			hsb = { brightness = 0.01 },
+		}
+	end
 end
 
 return { background }
