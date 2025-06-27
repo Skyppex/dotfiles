f1c5924191621eed0a33c0fba26ff4b47e016458
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-06-27 16:05:04 +0200

    fix(wezterm): use nu inside wsl inside nu

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -380,7 +380,7 @@ end)
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	-- config.front_end = "Software" -- OpenGL doesn't work quite well with RDP.
 
-	config.default_prog = { "wsl", "--cd", "~" }
+	config.default_prog = { "nu", "--execute", "wsl --cd '~'" }
 	config.default_cwd = "~"
 	table.insert(config.launch_menu, { label = "nu", args = { "nu" } })
 	table.insert(config.launch_menu, { label = "powershell", args = { "powershell.exe", "-NoLogo" } })
