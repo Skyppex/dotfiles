90c28249c7b5b4ff3b086871968d767fbf9e1d33
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-08-11 16:37:18 +0200

    feat(nvim): add map for GraffitiJoin

--- nvim/lua/skypex/custom/graffiti.lua
+++ nvim/lua/skypex/custom/graffiti.lua
@@ -2,5 +2,6 @@ require("graffiti").setup()
 
 local nmap = require("skypex.utils").nmap
 nmap("<leader>Gh", "<cmd>GraffitiHost<cr>", "Host graffiti session")
+nmap("<leader>Gj", "<cmd>GraffitiJoin<cr>", "Join graffiti session")
 nmap("<leader>Gq", "<cmd>GraffitiStop<cr>", "Stop graffiti session")
 nmap("<leader>GQ", "<cmd>GraffitiKill<cr>", "Kill graffiti server")
