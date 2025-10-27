8c76e08840d7a7708c6aba9caaff6e5bacbd0181
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-10-27 16:18:13 +0100

    feat(zen): use styled font and colorize more text by default

---
+++ zen-browser/user.js
@@ -0,0 +1,9 @@
+// Enable custom userChrome.css and userContent.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// (Optional) enable devtools chrome access for live editing
+user_pref("devtools.chrome.enabled", true);
+user_pref("devtools.debugger.remote-enabled", true);
+
+// (Optional) tweak UI reload behavior
+user_pref("layout.css.has-selector.enabled", true);
