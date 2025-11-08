b8b0bba6d92e092411a1fe5d835e932e106cc595
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-11-08 23:01:49 +0100

    feat(nvim): add snippet to quickly make a flake

--- nvim/lua/skypex/custom/snippets.lua
+++ nvim/lua/skypex/custom/snippets.lua
@@ -1,11 +1,100 @@
 local nmap = require("skypex.utils").nmap
 local ls = require("luasnip")
-local s, t, i = ls.snippet, ls.text_node, ls.insert_node
+local s, t, i, c = ls.snippet, ls.text_node, ls.insert_node, ls.choice_node
 local fmt = require("luasnip.extras.fmt").fmt
 local lsex = require("luasnip.extras")
 local rep = lsex.rep
 
 local M = {}
+
+local flake_snippet = t({
+	"{",
+	'  description = "";',
+	"",
+	"  inputs = {",
+	'    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";',
+	'    flake-utils.url = "github:numtide/flake-utils";',
+	'    #naersk.url = "github:nix-community/naersk";',
+	"    #fenix = {",
+	'    #  url = "github:nix-community/fenix";',
+	'    #  inputs.nixpkgs.follows = "nixpkgs";',
+	"    #};",
+	"  };",
+	"",
+	"  outputs = {",
+	"    self,",
+	"    nixpkgs,",
+	"    flake-utils,",
+	"    #naersk,",
+	"    #fenix,",
+	"    ...",
+	"  }:",
+	"    flake-utils.lib.eachDefaultSystem (system: let",
+	'      # system = "x86_64-linux";',
+	"      pkgs = import nixpkgs {inherit system;};",
+	"      #fenixLib = fenix.packages.${system};",
+	"      #toolchain = with fenixLib;",
+	"      #  combine [",
+	"      #    (stable.withComponents [",
+	'      #      "rustc"',
+	'      #      "cargo"',
+	'      #      "rustfmt"',
+	'      #      "clippy"',
+	'      #      "rust-src"',
+	'      #      "rust-docs"',
+	'      #      "rust-std"',
+	'      #      "rust-analyzer"',
+	"      #    ])",
+	"      #  ];",
+	"",
+	"      #naerskLib = (pkgs.callPackage naersk {}).override {",
+	"      #  cargo = toolchain;",
+	"      #  rustc = toolchain;",
+	"      #};",
+	"",
+	"      #pcpPackage = {release}:",
+	"      #  import ./default.nix {",
+	"      #    src = self;",
+	"      #    naersk = naerskLib;",
+	"      #    pkgConfig = pkgs.pkg-config;",
+	"      #    inherit release;",
+	"      #  };",
+	"",
+	"      #checks = import ./checks.nix {",
+	"      #  src = self;",
+	"      #  naersk = naerskLib;",
+	"      #  pkgs = pkgs;",
+	"      #};",
+	"",
+	"      #apps = import ./apps.nix {",
+	"      #  pkgs = pkgs;",
+	"      #};",
+	"    in {",
+	"      #packages = rec {",
+	"      #  default = debug;",
+	"      #  debug = pcpPackage {release = false;};",
+	"      #  release = pcpPackage {release = true;};",
+	"      #};",
+	"",
+	"      devShells.default = pkgs.mkShell {",
+	"        packages = [",
+	"          #toolchain",
+	"        ];",
+	'        #env.RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";',
+	"      };",
+	"",
+	"      #checks = checks;",
+	"",
+	"      #apps = apps;",
+	"",
+	"      #formatter = pkgs.writeShellApplication {",
+	'      #  name = "fmt";',
+	"      #  runtimeInputs = [pkgs.rustfmt pkgs.cargo];",
+	'      #  text = "cargo fmt --all";',
+	"      #};",
+	"    });",
+	"}",
+})
 
 local function all_snippets()
 	ls.add_snippets("all", {
@@ -23,6 +112,12 @@ local function cs_snippets()
 	ls.add_snippets("cs", {
 		s("sf", fmt("[SerializeField] private {1} {2};", { i(1, "Type"), i(2, "name") })),
 		s("sp", fmt("[field: SerializeField] public {1} {2} {{ get; set; }}", { i(1, "Type"), i(2, "name") })),
+	})
+end
+
+local function nix_snippets()
+	ls.add_snippets("nix", {
+		s("flake", flake_snippet),
 	})
 end
 
@@ -46,6 +141,7 @@ M.luasnip = function()
 	all_snippets()
 	js_snippets()
 	cs_snippets()
+	nix_snippets()
 
 	nmap("<leader><leader>s", "<cmd>lua require('skypex.custom.snippets').all()<cr>", "Source snippets")
 end
