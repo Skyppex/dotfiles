6688e3e391d1779e02609fc91890fb8400d02832
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-11-10 16:30:04 +0100

    fix(nvim): cloak ssh keys

--- nvim/lua/skypex/custom/cloak.lua
+++ nvim/lua/skypex/custom/cloak.lua
@@ -1,22 +1,30 @@
 local cloak = require("cloak")
 
 cloak.setup({
-	cloak_length = 8,
+	cloak_length = 5,
 	cloak_on_leave = true,
 	patterns = {
 		{
-			-- Match any file containing '.env'.
 			file_pattern = {
-				-- Match any file containing '.env'.
 				"*.env*",
 			},
-			-- Match an equals sign and any character after it.
-			-- This can also be a table of patterns to cloak,
-			-- example: cloak_pattern = { ':.+', '-.+' } for yaml files.
 			cloak_pattern = {
 				"=.+",
 				":.+",
 				"-.+",
+			},
+			-- A function, table or string to generate the replacement.
+			-- The actual replacement will contain the 'cloak_character'
+			-- where it doesn't cover the original text.
+			-- If left empty the legacy behavior of keeping the first character is retained.
+			replace = nil,
+		},
+		{
+			file_pattern = {
+				"*id_ed25519*",
+			},
+			cloak_pattern = {
+				".+",
 			},
 			-- A function, table or string to generate the replacement.
 			-- The actual replacement will contain the 'cloak_character'
