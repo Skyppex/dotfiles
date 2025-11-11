24936c26ddfe56dcecbb30a69f5b5e946101b4a8
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-11-11 20:41:58 +0100

    fix(nix): add rust toolchain from fenix

--- flake.nix
+++ flake.nix
@@ -4,19 +4,40 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
+    fenix,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       # system = "x86_64-linux";
       pkgs = import nixpkgs {inherit system;};
+
+      fenixLib = fenix.packages.${system};
+      rust-toolchain = with fenixLib;
+        combine [
+          (stable.withComponents [
+            "rustc"
+            "cargo"
+            "rustfmt"
+            "clippy"
+            "rust-src"
+            "rust-docs"
+            "rust-std"
+            "rust-analyzer"
+          ])
+        ];
     in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
+          rust-toolchain
           lua-language-server
           stylua
           yamlfmt
