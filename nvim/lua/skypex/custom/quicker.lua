f7b6756b7b8d4c281d557b3bee07ffd65e24c40e
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-12-01 14:32:03 +0100

    fix(nvim): use more intuitive keybind for qf list navigation

--- nvim/lua/skypex/custom/quicker.lua
+++ nvim/lua/skypex/custom/quicker.lua
@@ -5,8 +5,8 @@ local quicker = require("quicker")
 quicker.setup({})
 
 -- Quickfix list navigation
-map("n", "<leader>qk", "<cmd>cprev<CR>zz")
-map("n", "<leader>qj", "<cmd>cnext<CR>zz")
+map("n", "åq", "<cmd>cprev<CR>zz")
+map("n", "æq", "<cmd>cnext<CR>zz")
 
 map("n", "<leader>tq", function()
 	quicker.toggle()
