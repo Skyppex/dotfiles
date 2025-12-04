2c0c94f2163aaf29cd9e6bfdea4e62d5b10a9330
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2025-12-04 19:35:37 +0100

    chore(nvim): disable codecompanion

--- nvim/lua/skypex/plugins/ai.lua
+++ nvim/lua/skypex/plugins/ai.lua
@@ -1,20 +1,20 @@
 return {
-	{
-		"olimorris/codecompanion.nvim",
-		opts = {},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			{
-				"ravitemer/mcphub.nvim",
-				build = "bundled_build.lua",
-				dependencies = {
-					"nvim-lua/plenary.nvim",
-				},
-			},
-		},
-		config = function()
-			require("skypex.custom.ai")
-		end,
-	},
+	-- {
+	-- 	"olimorris/codecompanion.nvim",
+	-- 	opts = {},
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 		{
+	-- 			"ravitemer/mcphub.nvim",
+	-- 			build = "bundled_build.lua",
+	-- 			dependencies = {
+	-- 				"nvim-lua/plenary.nvim",
+	-- 			},
+	-- 		},
+	-- 	},
+	-- 	config = function()
+	-- 		require("skypex.custom.ai")
+	-- 	end,
+	-- },
 }
