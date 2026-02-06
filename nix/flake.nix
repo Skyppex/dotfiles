7c17fb5e4aad2a318d0759247ac7b2bf8c93451c
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2026-02-06 09:09:17 +0100

    feat(nix): use eachDefaultSystem for profiles
    needed this after i started using an arm laptop at work

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
 
     fenix-flake = {
       url = "github:nix-community/fenix";
@@ -18,113 +19,91 @@
   outputs = {
     self,
     nixpkgs,
+    flake-utils,
     fenix-flake,
     zen-browser,
     ...
-  }: let
-    system = "x86_64-linux";
-
-    pkgsFree = import nixpkgs {
-      config.allowUnfree = false;
-      inherit system;
-    };
-
-    pkgsUnfree = import nixpkgs {
-      config.allowUnfree = true;
-      inherit system;
-    };
-
-    fenix = fenix-flake.packages.${system};
-
-    cli-tools = import ./cli-tools.nix {
-      inherit pkgsFree;
-      inherit pkgsUnfree;
-      inherit fenix;
-    };
-
-    cli-tools-work = import ./cli-tools.nix {
-      inherit pkgsFree;
-      inherit pkgsUnfree;
-      inherit fenix;
-      dotnetSdks = with pkgsFree.dotnetCorePackages; [sdk_8_0 sdk_9_0];
-    };
-
-    common-desktop-tools = import ./common-desktop-tools.nix {
-      inherit pkgsUnfree;
-    };
-
-    work-desktop-tools = import ./work-desktop-tools.nix {
-      inherit pkgsFree;
-      inherit pkgsUnfree;
-    };
-
-    home-desktop-tools = import ./home-desktop-tools.nix {
-      inherit pkgsUnfree;
-      inherit system;
-      inherit zen-browser;
-    };
-
-    surface-laptop-tools = import ./home-desktop-tools.nix {
-      inherit pkgsUnfree;
-    };
-  in {
-    lib.${system}.packages = {
-      default = self.lib.${system}.packages.shell;
-      shell = cli-tools.packages;
-      work = cli-tools-work.packages ++ common-desktop-tools ++ work-desktop-tools;
-      home = cli-tools.packages ++ common-desktop-tools ++ home-desktop-tools;
-      surface = cli-tools.packages ++ surface-laptop-tools;
-    };
-
-    packages.${system} = {
-      default = self.packages.${system}.shell;
-
-      shell = pkgsUnfree.buildEnv {
-        name = "cli-tools";
-        paths = self.lib.${system}.packages.shell;
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgsFree = import nixpkgs {
+        config.allowUnfree = false;
+        inherit system;
       };
 
-      work = pkgsUnfree.buildEnv {
-        name = "work-tools";
-        paths = self.lib.${system}.packages.work;
-        nativeBuildInputs = [pkgsUnfree.makeWrapper];
-        buildInputs = [pkgsUnfree.libglvnd pkgsUnfree.mesa];
-        postBuild = ''
-          wrapProgram $out/bin/wezterm \
-              --prefix LD_LIBRARY_PATH : ${pkgsUnfree.libglvnd}/lib \
-              --prefix LD_LIBRARY_PATH : ${pkgsUnfree.mesa}/lib
-        '';
+      pkgsUnfree = import nixpkgs {
+        config.allowUnfree = true;
+        inherit system;
       };
 
-      home = pkgsUnfree.buildEnv {
-        name = "home-tools";
-        paths = self.lib.${system}.packages.home;
-        nativeBuildInputs = [pkgsUnfree.makeWrapper];
-        buildInputs = [pkgsUnfree.libglvnd pkgsUnfree.mesa];
-        postBuild = ''
-          wrapProgram $out/bin/wezterm \
-              --prefix LD_LIBRARY_PATH : ${pkgsUnfree.libglvnd}/lib \
-              --prefix LD_LIBRARY_PATH : ${pkgsUnfree.mesa}/lib
-        '';
+      fenix = fenix-flake.packages.${system};
+
+      cli-tools = import ./cli-tools.nix {
+        inherit pkgsFree;
+        inherit pkgsUnfree;
+        inherit fenix;
       };
 
-      surface = pkgsUnfree.buildEnv {
-        name = "surface-tools";
-        paths = self.lib.${system}.packages.surface;
-        nativeBuildInputs = [pkgsUnfree.makeWrapper];
-        buildInputs = [pkgsUnfree.libglvnd pkgsUnfree.mesa];
-        postBuild = ''
-          wrapProgram $out/bin/wezterm \
-              --prefix LD_LIBRARY_PATH : ${pkgsUnfree.libglvnd}/lib \
-              --prefix LD_LIBRARY_PATH : ${pkgsUnfree.mesa}/lib
-        '';
+      cli-tools-work = import ./cli-tools.nix {
+        inherit pkgsFree;
+        inherit pkgsUnfree;
+        inherit fenix;
+        dotnetSdks = with pkgsFree.dotnetCorePackages; [sdk_8_0 sdk_9_0];
       };
-    };
 
-    devShells.${system} = {
-      default = cli-tools.devShells.default;
-      lite = cli-tools.devShells.lite;
-      free = cli-tools.devShells.free;
-    };
-  };
+      common-desktop-tools = import ./common-desktop-tools.nix {
+        inherit pkgsUnfree;
+      };
+
+      work-desktop-tools = import ./work-desktop-tools.nix {
+        inherit pkgsFree;
+        inherit pkgsUnfree;
+      };
+
+      home-desktop-tools = import ./home-desktop-tools.nix {
+        inherit pkgsUnfree;
+        inherit system;
+        inherit zen-browser;
+      };
+
+      surface-laptop-tools = import ./home-desktop-tools.nix {
+        inherit pkgsUnfree;
+      };
+
+      profiles = {
+        shell = cli-tools.packages;
+        work = cli-tools-work.packages ++ common-desktop-tools ++ work-desktop-tools;
+        home = cli-tools.packages ++ common-desktop-tools ++ home-desktop-tools;
+        surface = cli-tools.packages ++ surface-laptop-tools;
+      };
+
+      mkEnv = name: paths:
+        pkgsUnfree.buildEnv {
+          inherit name paths;
+          nativeBuildInputs = [pkgsUnfree.makeWrapper];
+          buildInputs = [pkgsUnfree.libglvnd pkgsUnfree.mesa];
+          postBuild = ''
+            if [ -e "$out/bin/wezterm" ]; then
+                wrapProgram "$out/bin/wezterm" \
+                    --prefix LD_LIBRARY_PATH : ${pkgsUnfree.libglvnd}/lib \
+                    --prefix LD_LIBRARY_PATH : ${pkgsUnfree.mesa}/lib
+            fi
+          '';
+        };
+    in {
+      profiles = profiles;
+
+      packages = {
+        default = mkEnv "cli-tools" profiles.shell;
+        shell = mkEnv "cli-tools" profiles.shell;
+        work = mkEnv "work-tools" profiles.work;
+        home = mkEnv "home-tools" profiles.home;
+        surface = mkEnv "surface-tools" profiles.surface;
+      };
+
+      devShells = {
+        default = cli-tools.devShells.default;
+        lite = cli-tools.devShells.lite;
+        free = cli-tools.devShells.free;
+      };
+    });
 }
