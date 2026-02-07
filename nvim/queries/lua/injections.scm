c3628d45ee184bc6f3d0c0df2cf40564817af44c
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2026-02-07 15:44:09 +0100

    feat(nvim): add language injections for string based on a comment for the lua language

---
+++ nvim/queries/lua/injections.scm
@@ -0,0 +1,43 @@
+((comment) @injection.language (#offset! @injection.language 0 9 0 0)
+    (string
+        (string_content) @injection.content))
+
+((comment) @injection.language (#offset! @injection.language 0 9 0 0)
+    (_
+        (string
+            (string_content) @injection.content)))
+
+((comment) @injection.language (#offset! @injection.language 0 9 0 0)
+    (_ (_
+        (string
+            (string_content) @injection.content))))
+
+((comment) @injection.language (#offset! @injection.language 0 9 0 0)
+    (_ (_ (_
+        (string
+            (string_content) @injection.content)))))
+
+((comment) @injection.language (#offset! @injection.language 0 9 0 0)
+    (_ (_ (_ (_
+        (string
+            (string_content) @injection.content))))))
+
+((comment) @injection.language (#offset! @injection.language 0 9 0 0)
+    (_ (_ (_ (_ (_
+        (string
+            (string_content) @injection.content)))))))
+
+((comment) @injection.language (#offset! @injection.language 0 9 0 0)
+    (_ (_ (_ (_ (_ (_
+        (string
+            (string_content) @injection.content))))))))
+
+((comment) @injection.language (#offset! @injection.language 0 9 0 0)
+    (_ (_ (_ (_ (_ (_ (_
+        (string
+            (string_content) @injection.content)))))))))
+
+((comment) @injection.language (#offset! @injection.language 0 9 0 0)
+    (_ (_ (_ (_ (_ (_ (_ (_
+        (string
+            (string_content) @injection.content))))))))))
