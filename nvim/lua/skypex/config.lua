ce23b4ba00928264a96ff2d77c90bdf4c1994b84
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2026-02-12 12:44:11 +0100

    fix(nvim): remove tabline

--- nvim/lua/skypex/config.lua
+++ nvim/lua/skypex/config.lua
@@ -12,6 +12,7 @@ end
 
 vim.opt.cmdheight = 0
 vim.opt.winborder = "rounded"
+vim.opt.showtabline = 0
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
