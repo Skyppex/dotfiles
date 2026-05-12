ee9db2e6536b4338061188961cd36d1c530951e0
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2026-05-12 09:37:53 +0200

    feat(nvim): configure keys for graffiti

--- nvim/lua/skypex/custom/graffiti.lua
+++ nvim/lua/skypex/custom/graffiti.lua
@@ -1,4 +1,7 @@
-require("graffiti").setup()
+require("graffiti").setup({
+	authorized_keys = "~/.ssh/authorized_keys",
+	client_key = "~/.ssh/id_25519-self",
+})
 
 local map = require("skypex.utils").map
 map("n", "<leader>Gh", "<cmd>GraffitiHost<cr>", "Host graffiti session")
