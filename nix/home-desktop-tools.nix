c7aaff76f548487fa569583f3f5cc3e0cdf41750
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2026-05-14 22:56:28 +0200

    fix(nix): remove bottles & update flake

--- nix/home-desktop-tools.nix
+++ nix/home-desktop-tools.nix
@@ -3,15 +3,11 @@
   system,
   zen-browser,
 }: let
-  bottles = pkgsUnfree.bottles.override {
-    removeWarningPopup = true;
-  };
 in
   with pkgsUnfree; [
     ani-cli
     ani-skip
     blender
-    bottles
     catt
     prismlauncher
     r2modman
