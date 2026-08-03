0bc37b001d95e78dcf4a53835f4303a3dfc879c8
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2026-08-03 09:14:18 +0200

    fix(nvim): use full hl group string for graffiti cursor

--- nvim/lua/skypex/configs/graffiti.lua
+++ nvim/lua/skypex/configs/graffiti.lua
@@ -14,13 +14,20 @@ if not bin_path:is_file() then
 	end
 end
 
+local utils = require("skypex.utils")
+
+local colors = require("skypex.colors")
+
 require("graffiti").setup({
 	server_executable = bin_path.filename,
 	authorized_keys = "~/.ssh/authorized_keys",
 	client_key = "~/.ssh/id_25519-self",
+	cursors = {
+		hi1 = "guifg=" .. colors.background1 .. " guibg=" .. colors.dark_yellow,
+	},
 })
 
-local map = require("skypex.utils").map
+local map = utils.map
 map("n", "<leader>Gh", "<cmd>GraffitiHost<cr>", "Host graffiti session")
 map("n", "<leader>Gj", "<cmd>GraffitiJoin<cr>", "Join graffiti session")
 map("n", "<leader>Gq", "<cmd>GraffitiStop<cr>", "Stop graffiti session")
