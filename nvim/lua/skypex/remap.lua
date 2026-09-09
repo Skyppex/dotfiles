7fa0e4a441f29f0f5c85d09b477577f183921150
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2026-09-09 09:27:46 +0200

    chore(nvim): delete commented code

--- nvim/lua/skypex/remap.lua
+++ nvim/lua/skypex/remap.lua
@@ -184,9 +184,6 @@ map("n", "<leader>ih", function()
 	vim.notify(vim.inspect(vim.treesitter.get_captures_at_cursor(0)))
 end)
 
--- -- Replace operation (expr mapping for register support)
--- map("nx", "R", require("skypex.replace").replace, "replace", true)
-
 -- want this to allow me to replace from a register instead of typing it out in insert mode
 map("n", "RC", '"_c', "change without writing register")
 map("n", "RP", require("skypex.replace").replace, "change without writing register")
